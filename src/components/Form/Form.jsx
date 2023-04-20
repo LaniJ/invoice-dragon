6c9a0369610ba33b444ecd2c861fde7e98978222
Lani <ooluwalani@gmail.com>
2023-04-20 10:27:46 -0400

    modifies form component to pass correct name and id attributes to individual input fields

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -16,12 +16,20 @@ const Form = ({ onBizMod, onEmailMod, onPreviewToggle }) => {
     onEmailMod(name, value);
   }
 
-  const handleChangeB = (e) => {
+  // const handleChange = ({ target: { name, value }}) => {
+  //   e.preventDefault();
+  //   const name = e.target.name;
+  //   const value = e.target.value;
+  //   console.log('new change', name, value);
+  //   onEmailMod(name, value);
+  // }
+
+  // const handleChangeB = (e) => {
     // e.preventDefault();
     // console.log(e.target.value);
-    onBizMod(e.target.value);
+    // onBizMod(e.target.value);
     // setBusinessName(e.target.value);
-  }
+  // }
   const toggleViews = () => {
     onPreviewToggle();
   }
@@ -57,14 +65,14 @@ const Form = ({ onBizMod, onEmailMod, onPreviewToggle }) => {
               placeholder="Logo"
               onChange={handleChange}
             />
-            <div>
+            {/* <div>
               <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="image" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                 <path fill="currentColor" d="M464 448H48c-26.51 0-48-21.49-48-48V112c0-26.51 21.49-48 48-48h416c26.51 0 48 21.49 48 48v288c0 26.51-21.49 48-48 48zM112 120c-30.928 0-56 25.072-56 56s25.072 56 56 56 56-25.072 56-56-25.072-56-56-56zM64 384h384V272l-87.515-87.515c-4.686-4.686-12.284-4.686-16.971 0L208 320l-55.515-55.515c-4.686-4.686-12.284-4.686-16.971 0L64 336v48z"></path>
               </svg>
               <span>+ Logo</span>
               <span className={styles.upload__message}>Uploading...</span>
               <span className={styles.guide__message}>Add logo</span>
-            </div>
+            </div> */}
           </div>
         </div>
         <div className={styles.invoice__details}>
@@ -149,12 +157,12 @@ const Form = ({ onBizMod, onEmailMod, onPreviewToggle }) => {
               />
             </div>
             <div className={styles.form__field}>
-              <label htmlFor="Owner" className={styles.label}>Owner</label>
+              <label htmlFor="owner" className={styles.label}>Owner</label>
               <input
                 className={styles.input__default}
                 type="text" 
-                name="Owner" 
-                id="Owner" 
+                name="owner" 
+                id="owner" 
                 placeholder="Business owner name"
                 onChange={handleChange}
               />
@@ -187,45 +195,45 @@ const Form = ({ onBizMod, onEmailMod, onPreviewToggle }) => {
                 />
             </div>
             <div className={styles.form__field}>
-              <label htmlFor="address" className={styles.label}>Address</label>
+              <label htmlFor="clientAddress" className={styles.label}>Address</label>
               <input
                 className={styles.input__default}
                 type="text" 
-                name="address" 
-                id="address" 
+                name="clientAddress" 
+                id="clientAddress" 
                 placeholder="street"
                 onChange={handleChange}
               />
             </div>
             <div className={styles.form__field}>
-              <label htmlFor="city" className={styles.label}></label>
+              <label htmlFor="clientCity" className={styles.label}></label>
               <input
                 className={styles.input__default}
                 type="text" 
-                name="city" 
-                id="city" 
+                name="clientCity" 
+                id="clientCity" 
                 placeholder="city, state"
                 onChange={handleChange}
               />
             </div>
             <div className={styles.form__field}>
-              <label htmlFor="zipcode" className={styles.label}></label>
+              <label htmlFor="clientZipcode" className={styles.label}></label>
               <input
                 className={styles.input__default}
                 type="text" 
-                name="zipcode" 
-                id="zipcode" 
+                name="clientZipcode" 
+                id="clientZipcode" 
                 placeholder="zipcode"
                 onChange={handleChange}
               />
             </div>
             <div className={styles.form__field}>
-              <label htmlFor="phone" className={styles.label}>Phone</label>
+              <label htmlFor="clientPhone" className={styles.label}>Phone</label>
               <input
                 className={styles.input__default}
                 type="tel" 
-                name="phone" 
-                id="phone" 
+                name="clientPhone" 
+                id="clientPhone" 
                 placeholder="(123) 456 789"
                 onChange={handleChange}
               />
@@ -248,23 +256,23 @@ const Form = ({ onBizMod, onEmailMod, onPreviewToggle }) => {
             </div>
             {/* change placeholder to be toady's date by default */}
             <div className={styles.form__field}>
-              <label htmlFor="terms" className={styles.label}>Date</label>
+              <label htmlFor="date" className={styles.label}>Date</label>
               <input
                 className={styles.input__default}
                 type="text" 
-                name="terms" 
-                id="terms" 
+                name="date" 
+                id="date" 
                 placeholder="Apr 11, 2023"
                 onChange={handleChange}
               />
             </div>
             <div className={styles.form__field}>
-              <label htmlFor="terms" className={styles.label}>Terms</label>
+              <label htmlFor="clientWebsite" className={styles.label}>Terms</label>
               <input
                 className={styles.input__default}
                 type="text" 
-                name="terms" 
-                id="terms" 
+                name="clientWebsite" 
+                id="clientWebsite" 
                 placeholder="https://example-website.com"
                 onChange={handleChange}
               />
@@ -352,6 +360,44 @@ const Form = ({ onBizMod, onEmailMod, onPreviewToggle }) => {
         </div> */}
 
         <Table />
+        <div>
+          <p>Notes</p>
+          <textarea
+            name="notes" 
+            id="notes"
+            onChange={handleChange}
+            style={{height: '135px', marginTop: '18px', resize: 'none'}}
+            placeholder="Notes - any relevant information not covered, additional terms and conditions."
+            className={`${styles.input__default} ${styles.details}`}></textarea>
+        </div>
+        <section>
+          <span>
+            <p>Signature</p>
+          </span>
+          <span>
+            <button
+              type='button'
+              className={`${styles.add__invoice__item} ${styles.btn__add}`}>
+              {/* class="svg-inline--fa fa-plus fa-w-14"  */}
+              <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="plus" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="#ffffff" d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"></path></svg>
+            </button>
+          </span>
+        </section>
+
+        <section>
+          <p>Photos</p>
+          <input 
+            accept="image/jpeg ,image/jpg ,image/png ,image/webp"
+            type="file" 
+            name="photos" 
+            id="photos" 
+            placeholder="Logo"
+          />
+          
+        </section>
+
+        
+
 
         <button>Create Invoice</button>
       </form>
