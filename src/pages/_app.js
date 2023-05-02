7030c08c87e512bb358d5ab1fbcac9268d4aa243
Lani <ooluwalani@gmail.com>
2023-05-02 00:35:00 -0400

    adds new project font

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+import { Quicksand } from 'next/font/google';
+
+const quicksand = Quicksand({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={quicksand.className}>
+      <Component {...pageProps} />
+    </main>
+    // <Component {...pageProps} />
+  )
 }
