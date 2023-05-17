0cff932bf4aecbc8e27764522fff66ba33cbf50f
Lani <ooluwalani@gmail.com>
2023-05-16 23:28:07 -0400

    add layout component to wrap all app pages

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,4 @@
+import Layout from '@/components/Layout/Layout';
 import '@/styles/globals.css';
 import { Quicksand } from 'next/font/google';
 
@@ -5,8 +6,10 @@ const quicksand = Quicksand({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }) {
   return (
-    <main className={quicksand.className}>
-      <Component {...pageProps} />
-    </main>
+    <Layout>
+      <main className={quicksand.className}>
+        <Component {...pageProps} />
+      </main>
+    </Layout>
   )
 }
