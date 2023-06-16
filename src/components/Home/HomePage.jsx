7dc648eb19ba25c2e94c9db555c50ad25914a2ac
Lani <ooluwalani@gmail.com>
2023-06-16 12:07:20 -0400

    changes homepage text

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -8,7 +8,7 @@ const HomePage = () => {
       <div className={styles.main}>
         <h1 className={styles.text__lead}>Invoice Dragon</h1>
         <svg xmlns="http://www.w3.org/2000/svg" width="168" height="155" fill="#ffffff" viewBox="0 0 256 256"><path d="M216,40H40A16,16,0,0,0,24,56V208a8,8,0,0,0,11.58,7.15L64,200.94l28.42,14.21a8,8,0,0,0,7.16,0L128,200.94l28.42,14.21a8,8,0,0,0,7.16,0L192,200.94l28.42,14.21A8,8,0,0,0,232,208V56A16,16,0,0,0,216,40ZM176,144H80a8,8,0,0,1,0-16h96a8,8,0,0,1,0,16Zm0-32H80a8,8,0,0,1,0-16h96a8,8,0,0,1,0,16Z"></path></svg>
-        <h2 className={styles.text__sub}>Effortlessly Create Invoices and Receipts.<br />Absolutely Free.</h2>
+        <h2 className={styles.text__sub}>A fast and convenient solution for effortlessly creating Invoices and Receipts.<br />Absolutely Free !</h2>
         <div className={styles.page__btns}>
           <Link href={`/templates`}>
             <button className={`${styles.btn} ${styles.btn__secondary}`}>GET STARTED</button>
